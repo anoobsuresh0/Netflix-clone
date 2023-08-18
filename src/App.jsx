77147b327693853e6a6c4a33e30a31293e599c71
Anoob Suresh <anoob.suresh@perleybrook.com>
2023-08-18 19:47:17 +0530

    finised up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import RowPost from './components/RowPost/RowPost'
-import {originals,action, comedy, horror} from './urls'
+import {originals,action, comedy, romance,documentry} from './urls'
 
 
 function App() {
@@ -16,8 +16,9 @@ function App() {
       <Banner/>
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall />
-      <RowPost url={comedy} title='Comedy 1' isSmall />
-      <RowPost url={horror} title='Horror' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={romance} title='Romance' isSmall />
+      <RowPost url={documentry} title='Documentary' isSmall />
     </div>
   )
 }
--- src/urls.jsx
+++ src/urls.jsx
@@ -2,6 +2,7 @@ import {API_KEY} from './constants/constants'
 export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
 export const comedy = `discover/movie?api_key=${API_KEY}&language=en-US&page=1&with_genres=35`
-export const horror = `/discover/movie/?api_key=${API_KEY}&with_genres=27&append_to_response=videos`
+export const romance = `discover/movie?api_key=${API_KEY}&language=en-US&page=1&with_genres=10749`
+export const documentry = `discover/movie?api_key=${API_KEY}&language=en-US&page=1&with_genres=99`
 
 

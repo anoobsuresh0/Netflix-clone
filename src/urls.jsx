c0da26b4d82a98f2b0e744464bf63bf8beb5fd7b
Anoob Suresh <anoob.suresh@perleybrook.com>
2023-08-18 19:38:58 +0530

    comedy list

--- src/urls.jsx
+++ src/urls.jsx
@@ -1,5 +1,7 @@
 import {API_KEY} from './constants/constants'
 export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
-export const comedy = `/discover/movie/?api_key=${API_KEY}&with_genres=35&append_to_response=videos`
+export const comedy = `discover/movie?api_key=${API_KEY}&language=en-US&page=1&with_genres=35`
 export const horror = `/discover/movie/?api_key=${API_KEY}&with_genres=27&append_to_response=videos`
+
+

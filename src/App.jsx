801ba3d58b20e10af422cddc00fc4a63c1a07381
Anoob Suresh <anoob.suresh@perleybrook.com>
2023-08-18 19:29:38 +0530

    finishing up the design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import RowPost from './components/RowPost/RowPost'
-import {originals,action} from './urls'
+import {originals,action, comedy, horror} from './urls'
 
 
 function App() {
@@ -16,6 +16,8 @@ function App() {
       <Banner/>
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
     </div>
   )
 }
